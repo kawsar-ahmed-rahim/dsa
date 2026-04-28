8116041805a0fc5ec6d234efaaf0befea009fc4b
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-04-28 20:12:59 +0600

    till now

--- dsa_assaignment/doubly_linked_list/deleteMiddle.c
+++ dsa_assaignment/doubly_linked_list/deleteMiddle.c
@@ -1,4 +1,3 @@
-//! doubly linked list: delete note at certain point using function
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,7 +7,8 @@ struct Node{
     struct Node *next;
 };
 
-void deleteAtPosition(struct Node **head, int position){
+// Delete at certain position
+void deletePosition(struct Node **head, int position){
 
     if(*head == NULL){
         printf("List Empty\n");
@@ -16,19 +16,33 @@ void deleteAtPosition(struct Node **head, int position){
     }
 
     struct Node *temp = *head;
+    int i;
 
-    for(int i=1; i<position && temp!=NULL; i++)
+    // Delete first node
+    if(position == 1){
+        *head = temp->next;
+
+        if(*head != NULL)
+            (*head)->prev = NULL;
+
+        free(temp);
+        return;
+    }
+
+    // Move to required position
+    for(i = 1; temp != NULL && i < position; i++){
         temp = temp->next;
+    }
 
+    // Invalid position
     if(temp == NULL){
         printf("Invalid Position\n");
         return;
     }
 
+    // Link previous node with next node
     if(temp->prev != NULL)
         temp->prev->next = temp->next;
-    else
-        *head = temp->next;
 
     if(temp->next != NULL)
         temp->next->prev = temp->prev;
@@ -36,6 +50,7 @@ void deleteAtPosition(struct Node **head, int position){
     free(temp);
 }
 
+// Display list
 void display(struct Node *head){
 
     while(head != NULL){
@@ -48,7 +63,22 @@ void display(struct Node *head){
 
 int main(){
 
-    struct Node *head = NULL;
+    struct Node *head = malloc(sizeof(struct Node));
+    head->data = 10;
+    head->prev = NULL;
+
+    head->next = malloc(sizeof(struct Node));
+    head->next->data = 20;
+    head->next->prev = head;
+
+    head->next->next = malloc(sizeof(struct Node));
+    head->next->next->data = 30;
+    head->next->next->prev = head->next;
+    head->next->next->next = NULL;
+
+    deletePosition(&head, 2);   // Delete node at position 2
+
+    display(head);
 
     return 0;
 }
